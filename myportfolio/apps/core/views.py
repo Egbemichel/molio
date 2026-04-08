7308a3c4326013e4de2656c4f5b2253e0b22c364
Joel-Fah <joelfah2003@gmail.com>
2026-04-08 05:49:33 +0100

    bug out

--- myportfolio/apps/core/views.py
+++ myportfolio/apps/core/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from apps.projects.models import Project
+from apps.projects.models import Project, Category
 
 def home(request):
-    projects = Project.objects.prefetch_related('tags').all()
-    return render(request, 'core/home.html', {'projects': projects})
+    projects = Project.objects.prefetch_related('technologies').all()
+    categories = Category.objects.prefetch_related('projects', 'projects__technologies').all()
+    return render(request, 'core/home.html', {
+        'projects': projects,
+        'categories': categories
+    })
